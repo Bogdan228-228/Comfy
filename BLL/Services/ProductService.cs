b3e224d4e3e477cc1332018430deab9f5b8e62cf
vaniaopr1sh <hrelokvanya@icloud.com>
2026-06-14 15:24:32 +0300

    Додано Create, Edit, Details для Product та вирішено конфлікти

--- BLL/Services/ProductService.cs
+++ BLL/Services/ProductService.cs
@@ -16,7 +16,13 @@ namespace BLL.Services
 
         public IEnumerable<Product> GetAllProducts() => _context.Products.Include(c => c.Category).ToList();
 
+<<<<<<< HEAD
         public Product? GetProductById(int id) => _context.Products.FirstOrDefault(p => p.Id == id);
+=======
+        public async Task<Product> GetProductByIdAsync(int id) => await _context.Products
+            .Include(p => p.Category)
+            .FirstOrDefaultAsync(p => p.Id == id);
+>>>>>>> 2e8ea6e988554a1fa6e4cdf29892f46c226dd840
 
         public async Task AddProductAsync(Product product)
         {
@@ -26,7 +32,7 @@ namespace BLL.Services
 
         public async Task DeleteProductAsync(int id)
         {
-            var product = GetProductById(id);
+            var product = await GetProductByIdAsync(id);
             if (product != null)
             {
                 _context.Products.Remove(product);
@@ -44,9 +50,27 @@ namespace BLL.Services
             return products.ToList();
         }
 
+<<<<<<< HEAD
         public async Task UpdateProductAsync(Product product)
         {
             _context.Products.Update(product);
+=======
+        public async Task EditProductAsync(Product updatedProduct)
+        {
+            var product = await GetProductByIdAsync(updatedProduct.Id);
+            if (product == null)
+            {
+                throw new ArgumentException("Product not found");
+            }
+
+            product.Name = updatedProduct.Name;
+            product.Description = updatedProduct.Description;
+            product.Price = updatedProduct.Price;
+            product.Quantity = updatedProduct.Quantity;
+            product.ImageUrl = updatedProduct.ImageUrl;
+            product.CategoryId = updatedProduct.CategoryId;
+
+>>>>>>> 2e8ea6e988554a1fa6e4cdf29892f46c226dd840
             await _context.SaveChangesAsync();
         }
     }
